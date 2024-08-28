38b3caa186aa806746f9ccce7dab227fd63a39a0
SatpalInfilogix <satpalinfilogix@gmail.com>
2024-08-28 16:58:15 +0530

    original source code

---
+++ platform/plugins/job-board/src/Http/Controllers/CollegePageController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Botble\JobBoard\Http\Controllers;
+
+use Botble\Base\Events\BeforeEditContentEvent;
+use Botble\Base\Events\CreatedContentEvent;
+use Botble\Base\Events\UpdatedContentEvent;
+use Botble\Base\Facades\Assets;
+use Botble\Base\Http\Actions\DeleteResourceAction;
+use Botble\Base\Http\Controllers\BaseController;
+use Botble\Base\Supports\Breadcrumb;
+use Botble\JobBoard\Forms\CollegePageForm;
+use Botble\JobBoard\Http\Requests\AjaxCompanyRequest;
+use Botble\JobBoard\Http\Requests\CompanyRequest;
+use Botble\JobBoard\Http\Resources\CompanyResource;
+use Botble\JobBoard\Models\Company;
+use Botble\JobBoard\Services\StoreCompanyAccountService;
+use Botble\Page\Tables\CollegePageTable;
+use Illuminate\Http\Request;
+
+class CollegePageController extends BaseController
+{
+    protected function breadcrumb(): Breadcrumb
+    {
+        return parent::breadcrumb()
+            ->add(trans('plugins/job-board::company.name'), route('companies.index'));
+    }
+
+    public function index(CollegePageTable $pageTable)
+    {
+        $this->pageTitle(trans('plugins/job-board::college.name'));
+
+        return $pageTable->renderTable();
+    }
+
+}
