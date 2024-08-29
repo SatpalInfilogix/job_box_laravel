a212faae00d478450a6190b33c598ea3beff8720
SatpalInfilogix <satpalinfilogix@gmail.com>
2024-08-29 14:36:58 +0530

    Updated routes for college pages as needed

--- platform/plugins/job-board/src/Http/Controllers/CollegePageController.php
+++ platform/plugins/job-board/src/Http/Controllers/CollegePageController.php
@@ -2,27 +2,19 @@
 
 namespace Botble\JobBoard\Http\Controllers;
 
-use Botble\Base\Events\BeforeEditContentEvent;
-use Botble\Base\Events\CreatedContentEvent;
-use Botble\Base\Events\UpdatedContentEvent;
-use Botble\Base\Facades\Assets;
-use Botble\Base\Http\Actions\DeleteResourceAction;
 use Botble\Base\Http\Controllers\BaseController;
 use Botble\Base\Supports\Breadcrumb;
-use Botble\JobBoard\Http\Requests\AjaxCompanyRequest;
-use Botble\JobBoard\Http\Requests\CompanyRequest;
-use Botble\JobBoard\Http\Resources\CompanyResource;
-use Botble\JobBoard\Models\Company;
-use Botble\JobBoard\Services\StoreCompanyAccountService;
+use Botble\Page\Forms\CollegePageForm;
 use Botble\Page\Tables\CollegePageTable;
-use Botble\Page\Forms\PageForm;
+use Botble\Page\Http\Requests\PageRequest;
+use Illuminate\Support\Facades\Auth;
 
 class CollegePageController extends BaseController
 {
     protected function breadcrumb(): Breadcrumb
     {
         return parent::breadcrumb()
-            ->add(trans('plugins/job-board::company.name'), route('companies.index'));
+            ->add(trans('Colleges'), route('companies.index'));
     }
 
     public function index(CollegePageTable $pageTable, $college)
@@ -34,10 +26,29 @@ class CollegePageController extends BaseController
         return $pageTable->renderTable();
     }
 
-    public function create($college){
+    public function create(){
         $this->pageTitle(trans('packages/page::pages.create'));
 
-        return PageForm::create()->renderForm();
+        return CollegePageForm::create()->renderForm();
     }
 
+    public function store(PageRequest $request, $college)
+    {
+        $form = CollegePageForm::create()->setRequest($request);
+
+        $form->saving(function (CollegePageForm $form) use ($college) {
+            $form
+                ->getModel()
+                ->fill([...$form->getRequest()->input(), 'user_id' => Auth::guard()->id()])
+                ->fill([...$form->getRequest()->input(), 'college_id' => $college])
+                ->save();
+        });
+
+
+        return $this
+            ->httpResponse()
+            ->setPreviousRoute('college.pages.index', ['college' => $college])
+            ->setNextRoute('college.pages.index', ['college' => $college])
+            ->withCreatedSuccessMessage();
+    }
 }
